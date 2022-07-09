be9b1c32f2b3cd812b6908252f2b864e7540e040
Alexander Wang <aw26@illinois.edu>
2022-07-09 15:57:41 -0500

    figured out how to find all pdf files

--- scrape.py
+++ scrape.py
@@ -1,15 +1,23 @@
 from bs4 import BeautifulSoup
 import requests
 
-r = requests.get('https://faculty.math.illinois.edu/~icontrer/MATH415_Ivan_S18.html')
+#source = requests.get('https://faculty.math.illinois.edu/~icontrer/MATH415_Ivan_S18.html').text
+#soup = BeautifulSoup(source, 'lxml')
 
 with open('math/math415.html') as html_file:
     soup = BeautifulSoup(html_file, 'lxml')
 
-match = soup.href
-print(match)
+for link in soup.find_all('a') : # finds all links using <a> tag
+    pdf = link.text
+    print(pdf)
 
-# print(soup.prettify())
+    print()
+
+#print(link)
+
+#print(soup.prettify())
 
 # print (r.text) for hmtl files
 # print(r.content) for images
+
+# how to differentiate between pdf file links and other links?
