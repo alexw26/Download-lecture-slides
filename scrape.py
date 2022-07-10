1b91ffdd0425596ee74044646e4868cfbb88c051
Alexander Wang <aw26@illinois.edu>
2022-07-10 01:30:23 -0500

    added new library

--- scrape.py
+++ scrape.py
@@ -1,16 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
+from urllib.parse import unquote
 
-#source = requests.get('https://faculty.math.illinois.edu/~icontrer/MATH415_Ivan_S18.html').text
-#soup = BeautifulSoup(source, 'lxml')
+source = requests.get('https://faculty.math.illinois.edu/~icontrer/MATH415_Ivan_S18.html').text
+soup = BeautifulSoup(source, 'lxml')
 
-with open('math/math415.html') as html_file:
-    soup = BeautifulSoup(html_file, 'lxml')
+#with open('math/math415.html') as html_file:
+#    soup = BeautifulSoup(html_file, 'lxml')
 
 for link in soup.find_all('a') : # finds all links using <a> tag
-    pdf = link.text
-    print(pdf)
-
+    url = link.get('href')
+    #print(url)
+    print(unquote(url))
     print()
 
 #print(link)
