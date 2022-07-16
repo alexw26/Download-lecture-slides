1b38d17826c5aeec8654b3cf9ea2d0cab1659068
Alexander Wang <aw26@illinois.edu>
2022-07-16 15:37:48 -0500

    finished

--- scrape.py
+++ scrape.py
@@ -2,37 +2,41 @@ from bs4 import BeautifulSoup
 import requests
 from urllib.parse import unquote
 
-#source = requests.get('https://faculty.math.illinois.edu/~icontrer/MATH415_Ivan_S18.html').text
-#soup = BeautifulSoup(source, 'lxml')
+source = requests.get('https://faculty.math.illinois.edu/~icontrer/MATH415_Ivan_S18.html').text
+soup = BeautifulSoup(source, 'lxml')
 
-with open('math/math415.html') as html_file:
-    soup = BeautifulSoup(html_file, 'lxml')
+# with open('math/math415.html') as html_file:
+#    soup = BeautifulSoup(html_file, 'lxml')
 
 substring_1 = "https://faculty.math.illinois.edu"
 substring_2 = ".pdf"
 
+# for loop adapted from https://www.youtube.com/watch?v=HDEvWfSk2So together with https://www.youtube.com/watch?v=ng2o98k983k 
 for link in soup.find_all('a') : # finds all links using <a> tag
-    url = link.get('href')
-    # print(url)
-    pdf = unquote(url)
-    if (substring_1 in pdf and substring_2 in pdf):
-        print(pdf)
-        # next step is instead of printing pdf out, 
-        # we need to download it onto our machine 
-        # (use url variable instead of pdf variable 
-        # since url is the actual url and pdf is a string representation of it)
 
-        # also, include naming as well?
+    url = link.get('href')
+    pdf = unquote(url) # transforms url (as a link) in a string called pdf
+
+    if (substring_1 in pdf and substring_2 in pdf): # checks to make sure we have correct pdf links
+        r = requests.get(url) # use "url" instead of "pdf" since "pdf" is a string representation of "url"
+        lecture_name = r.url[url.rfind('/')+1:] # gets the name of the pdf by finding the last '/' and including everything after that last '/'
+        
+        print(lecture_name)
+
+        with open('c:/users/samwa/eduweb_download_slides/math/math415/' + lecture_name, 'wb') as file:
+            for chunk in r.iter_content(chunk_size=10000):
+                if chunk: # checks to make sure chunk is not empty
+                    file.write(chunk)
 
         # questions to ask Bhavya
-        # 1. Naming of pdf files (since some lectures include multiple slides,
-        # and there are also review pdfs, not just lecture pdfs)?
-        # 2. Include non-lecture pdfs?
+        # 1. If same pdf link, uninclude it?
 
-        # watch the video on how to download pdfs to local machine
     else:
         print("non-pdf file")
+    
     print()
+
+
 
 # print(link)
 
@@ -40,5 +44,3 @@ for link in soup.find_all('a') : # finds all links using <a> tag
 
 # print (r.text) for hmtl files
 # print(r.content) for images
-
-# how to differentiate between pdf file links and other links?
